35342a927abc297b160d59eb420565e34f03e502
mtaylor33 <taylormjm3121@gmail.com>
2019-12-25 21:33:51 -0500

    Updated classifiers.

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Topic :: Database",
+        "Topic :: Other/Nonlisted Topic",
     ],
 )
